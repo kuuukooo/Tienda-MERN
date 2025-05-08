a1c8f7086dc0b4d78be8b75c0542a82c265ab168
José Ramírez <jwh743@gmail.com>
2025-05-08 09:50:16 -0300

    Proyecto base en nueva branch

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,11 @@ app.use(cors());
 app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirnname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  })
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 }
 
 app.listen(port, () => {
