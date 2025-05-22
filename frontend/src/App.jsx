6b0d94dc75fd07e183951ee2d0d0edbe22f1a70c
José Ramírez <jwh743@gmail.com>
2025-05-22 14:39:27 -0300

    Error de Tooltip corregido

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,16 @@ import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/HomePage.jsx'
 import Navbar from './components/Navbar.jsx'
 import CreatePage from './pages/CreatePage.jsx'
-import { TooltipProvider } from "./components/CursorTooltipContext";
 
 function App() {
 
   return (
     <Box minH={'100vh'} bg={useColorModeValue('gray.100', 'gray.800')} boxShadow={'md'} mb={4}>
-      <TooltipProvider>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/create' element={<CreatePage />} />
-        </Routes>
-      </TooltipProvider>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+      </Routes>
     </Box>
   )
 }
